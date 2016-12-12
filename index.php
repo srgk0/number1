0862ec32a2880f3ccbade18613be6bdbe0682896
srgk0 <ser414.93@mail.ru>
2016-12-12 09:37:33 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,31 +1,96 @@
-<?
-// Страница авторизации
-
-# Соединямся с БД
-$conn = new PDO("sqlsrv:server = tcp:sqldatabase2.database.windows.net,1433; Database = sqldatabase2", "makkdragonhawk", "makkDR3748");
-$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-if(isset($_POST['submit']))
-{
-# Вытаскиваем из БД запись, у которой логин равняеться введенному
-$query = query($conn,"SELECT name, passw FROM reg_table WHERE name='".real_escape_string($conn,$_POST['login'])."' LIMIT 1");
-$data = fetch_assoc($query);
-
-# Сравниваем пароли
-if($data['passw'] === $_POST['password'])
-{
-echo $data;
-print "Вы ввели правильный логин/пароль";
-}
-else
-{
-echo $data;
-print "Вы ввели неправильный логин/пароль";
-}
-}
-?>
-<form method="POST">
-Логин <input name="login" type="text"><br>
-Пароль <input name="password" type="password"><br>
-<input name="submit" type="submit" value="Войти">
+<html>
+<head>
+<Title>Registration Form</Title>
+<style type="text/css">
+    body { background-color:
+ #fff; border-top: solid 10px #000;
+ color: #333; font-size: .85em;
+ margin: 20; padding: 20;
+ font-family: "Segoe UI",
+ Verdana, Helvetica, Sans-Serif;
+    }
+    h1, h2, h3,{ color: #000; 
+margin-bottom: 0; padding-bottom: 0; }
+    h1 { font-size: 2em; }
+    h2 { font-size: 1.75em; }
+    h3 { font-size: 1.2em; }
+    table { margin-top: 0.75em; }
+    th { font-size: 1.2em;
+ text-align: left; border: none; padding-left: 0; }
+    td { padding: 0.25em 2em 0.25em 0em; 
+border: 0 none; }
+</style>
+</head>
+<body>
+<h1>Register here!</h1>
+<p>Fill in your name and 
+email address, then click <strong>Submit</strong> 
+to register.</p>
+<form method="post" action="index.php" 
+enctype="multipart/form-data" >
+      Name  <input type="text" 
+name="name" id="name"/></br>
+      Email <input type="text" 
+name="email" id="email"/></br>
+      <input type="submit" 
+name="submit" value="Submit" />
 </form>
+<?php
+// DB connection info
+$host = "tcp:srgk0.database.windows.net,1433";
+$user = "srgk0";
+$pwd = "priorar225os";
+$db = "first";
+// Connect to database.
+try {
+    $conn = new PDO("sqlsrv:server = tcp:srgk0.database.windows.net,1433; Database = first", "srgk0", "prios");
+    $conn->setAttribute
+( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+if(!empty($_POST)) {
+try {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $date = date("Y-m-d");
+    // Insert data
+    $sql_insert = 
+"INSERT INTO registration_tbl (name, email, date) 
+                   VALUES (?,?,?)";
+    $stmt = $conn->prepare($sql_insert);
+    $stmt->bindValue(1, $name);
+    $stmt->bindValue(2, $email);
+    $stmt->bindValue(3, $date);
+    $stmt->execute();
+}
+catch(Exception $e) {
+    die(var_dump($e));
+}
+echo "<h3>Your're registered!</h3>";
+}
+$sql_select = "SELECT * FROM registration_tbl";
+$stmt = $conn->query($sql_select);
+$registrants = $stmt->fetchAll(); 
+if(count($registrants) > 0) {
+    echo "<h2>People who are registered:</h2>";
+    echo "<table>";
+    echo "<tr><th>Name</th>";
+    echo "<th>Email</th>";
+    echo "<th>Date</th></tr>";
+    foreach($registrants as $registrant) {
+        echo "<tr><td>".$registrant['name']."</td>";
+        echo "<td>".$registrant['email']."</td>";
+        echo "<td>".$registrant['date']."</td></tr>";
+    }
+    echo "</table>";
+} else {
+    echo "<h3>No one is currently registered.</h3>";
+}
+
+?>
+</body>
+</html>
+
