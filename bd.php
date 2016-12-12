3fc1177c399c0f0603aed442e8d536731dad3608
srgk0 <ser414.93@mail.ru>
2016-12-12 10:13:59 +0300

    Create bd.php

---
+++ bd.php
@@ -0,0 +1,4 @@
+<?php
+    $db = mysql_connect ("sqlsrv:server = tcp:srgk01.database.windows.net,1433; Database = forzelen", "ytrewq", "QWERTYqwerty123");
+    mysql_select_db ("registration_tbl",$db);
+    ?>
