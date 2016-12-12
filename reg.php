a6abda711d75b055d12e3da8e530614185b1355f
srgk0 <ser414.93@mail.ru>
2016-12-12 10:21:14 +0300

    Update reg.php

--- reg.php
+++ reg.php
@@ -1,35 +1,24 @@
-<?php
-    if (isset($_POST['login'])) { $login = $_POST['login']; if ($login == '') { unset($login);} } //заносим введенный пользователем логин в переменную $login, если он пустой, то уничтожаем переменную
-    if (isset($_POST['password'])) { $password=$_POST['password']; if ($password =='') { unset($password);} }
-    //заносим введенный пользователем пароль в переменную $password, если он пустой, то уничтожаем переменную
- if (empty($login) or empty($password)) //если пользователь не ввел логин или пароль, то выдаем ошибку и останавливаем скрипт
-    {
-    exit ("Вы ввели не всю информацию, вернитесь назад и заполните все поля!");
-    }
-    //если логин и пароль введены, то обрабатываем их, чтобы теги и скрипты не работали, мало ли что люди могут ввести
-    $login = stripslashes($login);
-    $login = htmlspecialchars($login);
- $password = stripslashes($password);
-    $password = htmlspecialchars($password);
- //удаляем лишние пробелы
-    $login = trim($login);
-    $password = trim($password);
- // подключаемся к базе
-    include ("bd.php");// файл bd.php должен быть в той же папке, что и все остальные, если это не так, то просто измените путь 
- // проверка на существование пользователя с таким же логином
-    $result = mysql_query("SELECT id FROM users WHERE login='$login'",$db);
-    $myrow = mysql_fetch_array($result);
-    if (!empty($myrow['id'])) {
-    exit ("Извините, введённый вами логин уже зарегистрирован. Введите другой логин.");
-    }
- // если такого нет, то сохраняем данные
-    $result2 = mysql_query ("INSERT INTO users (login,password) VALUES('$login','$password')");
-    // Проверяем, есть ли ошибки
-    if ($result2=='TRUE')
-    {
-    echo "Вы успешно зарегистрированы! Теперь вы можете зайти на сайт. <a href='index.php'>Главная страница</a>";
-    }
- else {
-    echo "Ошибка! Вы не зарегистрированы.";
-    }
-    ?>
+<html>
+    <head>
+    <title>Регистрация</title>
+    </head>
+    <body>
+    <h2>Регистрация</h2>
+    <form action="save_user.php" method="post">
+    <!--**** save_user.php - это адрес обработчика.  То есть, после нажатия на кнопку "Зарегистрироваться", данные из полей  отправятся на страничку save_user.php методом "post" ***** -->
+<p>
+    <label>Ваш логин:<br></label>
+    <input name="login" type="text" size="15" maxlength="15">
+    </p>
+<!--**** В текстовое поле (name="login" type="text") пользователь вводит свой логин ***** -->
+<p>
+    <label>Ваш пароль:<br></label>
+    <input name="password" type="password" size="15" maxlength="15">
+    </p>
+<!--**** В поле для паролей (name="password" type="password") пользователь вводит свой пароль ***** --> 
+<p>
+    <input type="submit" name="submit" value="Зарегистрироваться">
+<!--**** Кнопочка (type="submit") отправляет данные на страничку save_user.php ***** --> 
+</p></form>
+    </body>
+    </html>
